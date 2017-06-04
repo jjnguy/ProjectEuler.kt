7ca22142044977ca4c41c8b10e7b5538900d3cc2
Justin Nelson <justin.john.nelson@gmail.com>
2017-06-04 17:18:54 -0500

    initial commit

---
+++ app/src/main/java/com/example/justi/eulerkt/ProblemList.kt
@@ -0,0 +1,12 @@
+package com.example.justi.eulerkt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProblemList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_problem_list)
+    }
+}
